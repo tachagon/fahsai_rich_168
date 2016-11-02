8c7d7345ab70973a94b9e87c558870d16af0c720
Tatchagon <o_k_t@hotmail.com>
2016-11-03 01:34:38 +0700

    gentelella on rails

---
+++ app/assets/javascripts/footermanifest.js
@@ -0,0 +1,13 @@
+
+
+
+//= require gentelella/production/js/maps/jquery-jvectormap-world-mill-en
+//= require gentelella/production/js/maps/jquery-jvectormap-us-aea-en
+//= require gentelella/production/js/maps/gdp-data
+
+
+//= require gentelella/build/js/custom.min
+//= require flash
+//= require gentelella_index
+
+
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-
+  	flash[:success] = "Welcome to Application Index"
   end
 end
